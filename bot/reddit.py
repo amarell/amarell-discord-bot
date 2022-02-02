6a521e7549ca37268823f324f79cf361b3e6a1a5
amarell <mujagicamar@gmail.com>
2022-02-02 21:29:05 +0100

    bunch of new reddit commands

--- bot/reddit.py
+++ bot/reddit.py
@@ -3,11 +3,12 @@ import discord
 import random
 import asyncpraw
 import datetime
+import os
 
 reddit = asyncpraw.Reddit(
-    client_id="zRz7g1nCkdYbL8954UCong",
-    client_secret="gqbxdWjbVqfnzmz0Ovyc0vGiaoJ8gA",
-    user_agent="discord:com.amarellBot:v1.0.0 (by /u/Snoopy34)",
+    client_id=os.environ["reddit-client-id"],
+    client_secret=os.environ["reddit-client-secret"],
+    user_agent=os.environ["reddit-user-agent"],
 )
 
 class Reddit(commands.Cog):
@@ -16,10 +17,11 @@ class Reddit(commands.Cog):
 
     @commands.command()
     async def wallpaper(self, ctx):
+        """Generates a random wallpaper from r/wallpapers"""
         wallpapers = []
         subreddit = await reddit.subreddit("wallpapers", fetch=True)
 
-        async for submission in subreddit.top(limit=100):
+        async for submission in subreddit.hot(limit=15):
             wallpapers.append(submission)
 
         random_wallpaper = random.choice(wallpapers)
@@ -42,10 +44,11 @@ class Reddit(commands.Cog):
         
     @commands.command()
     async def nature_wallpaper(self, ctx):
+        """Generates a random wallpaper from subreddit r/earthporn"""
         wallpapers = []
         subreddit = await reddit.subreddit("earthporn", fetch=True)
 
-        async for submission in subreddit.top(limit=100):
+        async for submission in subreddit.hot(limit=15):
             wallpapers.append(submission)
 
         random_wallpaper = random.choice(wallpapers)
@@ -86,13 +89,103 @@ class Reddit(commands.Cog):
 
         embed.set_image(url=random_artwork.url)
         embed.set_footer(
-                text="Wallpaper posted on reddit on",
+                text="Artwork posted on reddit on",
                 icon_url="https://www.redditinc.com/assets/images/site/reddit-logo.png"
             )
 
         await ctx.send(
                 content="Here is your random artwork from reddit\n" + 
                         "If you like it please consider supporting the artist", 
+                embed=embed
+            )
+
+    @commands.command()
+    async def meme(self, ctx):
+        """Displays a fresh meme from reddit"""
+        fresh_memes = []
+
+        subreddit = await reddit.subreddit("memes")
+
+        async for submission in subreddit.hot(limit=100):
+            fresh_memes.append(submission)
+
+        random_fresh_meme = random.choice(fresh_memes)
+
+        embed = discord.Embed(
+                title="Memes from r/memes",
+                colour=discord.Colour.dark_gold(), 
+                url="https://www.reddit.com" + random_fresh_meme.permalink, 
+                timestamp=datetime.datetime.utcfromtimestamp(random_fresh_meme.created_utc)
+            )
+
+        embed.set_image(url=random_fresh_meme.url)
+        embed.set_footer(
+                text="Meme posted on reddit on",
+                icon_url="https://www.redditinc.com/assets/images/site/reddit-logo.png"
+            )
+
+        await ctx.send(
+                content="Here is your random meme from reddit\n", 
+                embed=embed
+            )
+
+    @commands.command()
+    async def anarchy(self, ctx):
+        """Displays a fresh meme from one of my favorite subreddits r/AnarchyChess"""
+        horsey_memes = []
+
+        subreddit = await reddit.subreddit("anarchychess")
+
+        async for submission in subreddit.hot(limit=100):
+            horsey_memes.append(submission)
+
+        random_horsey_meme = random.choice(horsey_memes)
+
+        embed = discord.Embed(
+                title="Memes from r/AnarchyChess",
+                colour=discord.Colour.dark_gold(), 
+                url="https://www.reddit.com" + random_horsey_meme.permalink, 
+                timestamp=datetime.datetime.utcfromtimestamp(random_horsey_meme.created_utc)
+            )
+
+        embed.set_image(url=random_horsey_meme.url)
+        embed.set_footer(
+                text="Meme posted on reddit on",
+                icon_url="https://www.redditinc.com/assets/images/site/reddit-logo.png"
+            )
+
+        await ctx.send(
+                content="Here is your random meme from Anarchy Chess\n", 
+                embed=embed
+            )
+    
+    @commands.command()
+    async def whenthe(self, ctx):
+        """Displays a fresh meme from one of my favorite subreddits r/whenthe"""
+        whenthe_memes = []
+
+        subreddit = await reddit.subreddit("whenthe")
+
+        async for submission in subreddit.hot(limit=100):
+            whenthe_memes.append(submission)
+
+        random_whenthe_meme = random.choice(whenthe_memes)
+
+        embed = discord.Embed(
+                title="Memes from r/whenthe",
+                colour=discord.Colour.dark_gold(), 
+                url="https://www.reddit.com" + random_whenthe_meme.permalink, 
+                timestamp=datetime.datetime.utcfromtimestamp(random_whenthe_meme.created_utc)
+            )
+
+        embed.set_image(url=random_whenthe_meme.url)
+        embed.set_footer(
+                text="Meme posted on reddit on",
+                icon_url="https://www.redditinc.com/assets/images/site/reddit-logo.png"
+            )
+
+        await ctx.send(
+                content="Here is your random meme from r/whenthe:\n", 
                 embed=embed
             )
 

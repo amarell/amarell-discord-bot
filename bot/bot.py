1164f58c92888b5400c4724731d02d0491172fc3
amarell <mujagicamar@gmail.com>
2022-02-02 20:50:58 +0100

    added reddit Cog

--- bot/bot.py
+++ bot/bot.py
@@ -3,19 +3,8 @@
 import datetime
 import discord
 from discord.ext import commands
-import vars
-import asyncpraw
 import random
 import os
-
-reddit = asyncpraw.Reddit(
-    client_id="zRz7g1nCkdYbL8954UCong",
-    client_secret="gqbxdWjbVqfnzmz0Ovyc0vGiaoJ8gA",
-    user_agent="discord:com.amarellBot:v1.0.0 (by /u/Snoopy34)",
-)
-
-
-
 
 
 description = '''An example bot to showcase the discord.ext.commands extension
@@ -29,8 +18,7 @@ bot = commands.Bot(command_prefix='-', description=description, intents=intents)
 # Here we are going to add all the extensions (external .py files)
 bot.load_extension('bot_commands')
 bot.load_extension('wiki')
-bot.reload_extension('wiki')
-
+bot.load_extension('reddit')
 
 @bot.event
 async def on_ready():
@@ -38,55 +26,5 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-
-@bot.command()
-async def wallpaper(ctx):
-    wallpapers = []
-    subreddit = await reddit.subreddit("wallpapers", fetch=True)
-
-    async for submission in subreddit.top(limit=100):
-        wallpapers.append(submission)
-
-    random_wallpaper = random.choice(wallpapers)
-
-    embed = discord.Embed(
-            title="Wallpapers from r/wallpapers",
-            colour=discord.Colour.orange(), 
-            url="https://www.reddit.com" + random_wallpaper.permalink, 
-            timestamp=datetime.datetime.utcfromtimestamp(random_wallpaper.created_utc)
-        )
-
-    embed.set_image(url=random_wallpaper.url)
-    embed.set_footer(
-            text="Wallpaper posted on reddit on",
-            icon_url="https://www.redditinc.com/assets/images/site/reddit-logo.png"
-        )
-
-    await ctx.send(content="Here is your random wallpaper:", embed=embed)
-
-@bot.command()
-async def nature_wallpaper(ctx):
-    wallpapers = []
-    subreddit = await reddit.subreddit("earthporn", fetch=True)
-
-    async for submission in subreddit.top(limit=100):
-        wallpapers.append(submission)
-
-    random_wallpaper = random.choice(wallpapers)
-
-    embed = discord.Embed(
-            title="Wallpapers from r/wallpapers",
-            colour=discord.Colour.orange(), 
-            url="https://www.reddit.com" + random_wallpaper.permalink, 
-            timestamp=datetime.datetime.utcfromtimestamp(random_wallpaper.created_utc)
-        )
-
-    embed.set_image(url=random_wallpaper.url)
-    embed.set_footer(
-            text="Wallpaper posted on reddit on",
-            icon_url="https://www.redditinc.com/assets/images/site/reddit-logo.png"
-        )
-
-    await ctx.send(content="Here is your random wallpaper:", embed=embed)
     
 bot.run(os.environ["discord-api-token"])
--- bot/reddit.py
+++ bot/reddit.py
@@ -1,6 +1,8 @@
-from re import sub
-import asyncpraw
+from discord.ext import commands
+import discord
 import random
+import asyncpraw
+import datetime
 
 reddit = asyncpraw.Reddit(
     client_id="zRz7g1nCkdYbL8954UCong",
@@ -8,15 +10,63 @@ reddit = asyncpraw.Reddit(
     user_agent="discord:com.amarellBot:v1.0.0 (by /u/Snoopy34)",
 )
 
-print(reddit.read_only)
-async def getsubreddit():
-    subreddit = await reddit.subreddit("redditdev", fetch=True)
-    print(subreddit.display_name)
+class Reddit(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
+    @commands.command()
+    async def wallpaper(self, ctx):
+        wallpapers = []
+        subreddit = await reddit.subreddit("wallpapers", fetch=True)
 
+        async for submission in subreddit.top(limit=100):
+            wallpapers.append(submission)
 
-# Output: redditdev
+        random_wallpaper = random.choice(wallpapers)
 
+        embed = discord.Embed(
+                title="Wallpapers from r/wallpapers",
+                colour=discord.Colour.orange(), 
+                url="https://www.reddit.com" + random_wallpaper.permalink, 
+                timestamp=datetime.datetime.utcfromtimestamp(random_wallpaper.created_utc)
+            )
+
+        embed.set_image(url=random_wallpaper.url)
+        embed.set_footer(
+                text="Wallpaper posted on reddit on",
+                icon_url="https://www.redditinc.com/assets/images/site/reddit-logo.png"
+            )
+
+        await ctx.send(content="Here is your random wallpaper:", embed=embed)
+
+        
+    @commands.command()
+    async def nature_wallpaper(self, ctx):
+        wallpapers = []
+        subreddit = await reddit.subreddit("earthporn", fetch=True)
+
+        async for submission in subreddit.top(limit=100):
+            wallpapers.append(submission)
+
+        random_wallpaper = random.choice(wallpapers)
+
+        embed = discord.Embed(
+                title="Wallpapers from r/wallpapers",
+                colour=discord.Colour.orange(), 
+                url="https://www.reddit.com" + random_wallpaper.permalink, 
+                timestamp=datetime.datetime.utcfromtimestamp(random_wallpaper.created_utc)
+            )
+
+        embed.set_image(url=random_wallpaper.url)
+        embed.set_footer(
+                text="Wallpaper posted on reddit on",
+                icon_url="https://www.redditinc.com/assets/images/site/reddit-logo.png"
+            )
+
+        await ctx.send(content="Here is your random wallpaper:", embed=embed)
+
+def setup(bot):
+    bot.add_cog(Reddit(bot))
 
 
 

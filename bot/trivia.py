6b4e4597b352033a7cc0aa020c55af76ef681e74
amarell <mujagicamar@gmail.com>
2022-03-12 18:12:25 +0100

    functional game of trivia

--- bot/trivia.py
+++ bot/trivia.py
@@ -3,6 +3,8 @@ import requests
 from discord.ext import commands
 import json
 import discord
+import asyncio
+import random
 
 class Trivia(commands.Cog):
     def __init__(self, bot):
@@ -22,16 +24,62 @@ class Trivia(commands.Cog):
 
             print(trivia_question['results'][0])
 
+            category = trivia_question['results'][0]['category']
             question = trivia_question['results'][0]['question']
-            reformated_question = question.replace("&quot;", "\"")
+            question_cleanup1 = question.replace("&quot;", "\"")
+            question_cleanup2 = question_cleanup1.replace("&#039;", "\'")
+            
+            incorrect_answers = trivia_question['results'][0]['incorrect_answers']
+            correct_answer = trivia_question['results'][0]['correct_answer']
+            correct_answer_cleanup1 = correct_answer.replace("&quot;", "\"")
+            correct_answer_cleanup2 = correct_answer_cleanup1.replace("&#039;", "\'")
 
-            embed = discord.Embed(colour=discord.Colour(0xd15659), description=reformated_question)
-            embed.add_field(name="A", value=trivia_question['results'][0]['correct_answer'], inline=False)
-            embed.add_field(name="B", value="incorrect 1", inline=False)
-            embed.add_field(name="C", value="incorrect 2", inline=False)
-            embed.add_field(name="D", value="incorrect 3", inline=False)
+            
+            answers = []
+            answers.append(correct_answer_cleanup2)
 
+            for item in incorrect_answers:
+                incorrect_answer_cleanup1 = item.replace("&quot;", "\"")
+                incorrect_answer_cleanup2 = incorrect_answer_cleanup1.replace("&#039;", "\'")
+                answers.append(incorrect_answer_cleanup2)
+
+            random.shuffle(answers)
+
+            position_of_correct_answer = answers.index(correct_answer_cleanup2)
+            if(position_of_correct_answer == 0):
+                correct_letter = 'a'
+            elif(position_of_correct_answer == 1):
+                correct_letter = 'b'
+            elif(position_of_correct_answer == 2):
+                correct_letter = 'c'
+            elif(position_of_correct_answer == 3):
+                correct_letter = 'd'
+
+            print(answers)
+            print(correct_letter)
+
+            embed = discord.Embed(colour=discord.Colour(0xd15659), description="**" + question_cleanup2 + "**")
+            embed.add_field(name="Category", value="```" + category + "```", inline=True)
+            embed.add_field(name="Difficulty", value="```" + difficulty.capitalize() + "```", inline=True)
+            embed.add_field(name="A", value=answers[0], inline=False)
+            embed.add_field(name="B", value=answers[1], inline=False)
+            embed.add_field(name="C", value=answers[2], inline=False)
+            embed.add_field(name="D", value=answers[3], inline=False)
             await ctx.send(embed=embed)
+
+            try: 
+                msg = await self.bot.wait_for('message', timeout=10)
+                if(len(msg.content) == 1):
+                    if(msg.content.lower() == correct_letter):
+                        await ctx.send("That's right! Congratulations!")
+                    else:
+                        await ctx.send("Close but no cigar! The correct answer is " + correct_letter)
+
+
+            except asyncio.TimeoutError:
+                await ctx.send("Time's up!\nThe correct answer was: " + correct_letter)
+
+            
 
 
 def setup(bot):
